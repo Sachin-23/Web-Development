e589e190bbf62308dd8e5e3e08d3253aee1e4fd0
Sachin-23 <Smudaliyar8452091824@gmail.com>
2020-10-24 15:15:00 +0530

    add remove unnecessary files

--- capstone/songs/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    is_artist = models.BooleanField("artist", default=False) # for artist
