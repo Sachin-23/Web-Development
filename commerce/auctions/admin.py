767ec65ae56feeaae2f17d38aec244c0ea9d3235
Sachin-23 <Smudaliyar8452091824@gmail.com>
2020-11-15 13:49:58 +0530

    remove duplicate files

--- commerce/auctions/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from auctions.models import *
-# Register your models here.
-
-admin.site.register(Auction_listing)
-admin.site.register(Bids)
-admin.site.register(Comments)
--- project4/network/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-
-from network.models import *
-# Register your models here.
-
-admin.site.register(Post)
-admin.site.register(Like)
-admin.site.register(Follower)
